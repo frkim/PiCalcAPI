f10ca746f71f7e8e62d5753a884f68753d2b8815
fxkim <frkim@live.fr>
2024-07-08 18:59:51 +0200

    update

--- Utils/obj/Debug/net8.0/Utils.AssemblyInfo.cs
+++ Utils/obj/Debug/net8.0/Utils.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b6a310436bbe52b98ebd50678abcf932942236")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e16306b38c6e05afbffa1b652898f6737ba41af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
